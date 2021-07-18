2c64ea5951c92e978669140105c956d3498878ab
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-07-18 15:37:31 +0200

    adding node entrypoint

---
+++ core/node.py
@@ -0,0 +1,11 @@
+# the core node entrypoint for HyperKube
+version_minor = 0
+version_major = 0
+version_patch = 1
+
+version_string = "0.0.1"
+version_status = "alpha"
+
+import os
+import sqlite3
+from sys import version_info
